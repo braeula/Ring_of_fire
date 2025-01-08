b1bf5b44df07ba07024b4313843f99260847d183
Arthur Bräul <a.braeul@live.de>
2025-01-08 23:56:26 +0100

    start-button

--- src/app/start-screen/start-screen.component.ts
+++ src/app/start-screen/start-screen.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-start-screen',
@@ -8,5 +9,12 @@ import { Component } from '@angular/core';
   styleUrl: './start-screen.component.scss'
 })
 export class StartScreenComponent {
+
+  constructor(private router: Router){}
+
+  newGame(){
+    // Start game
+    this.router.navigateByUrl('/game')
+  }
 
 }
