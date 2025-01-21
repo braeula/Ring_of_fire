0b6ccc5c293733fb7a97b1fa5d909ffa3111f01f
Arthur Bräul <a.braeul@live.de>
2025-01-21 16:42:09 +0100

    show card, start-game

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,12 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Game } from '../../models/game';
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
 export class GameComponent {
+  pickCardAnimation = false
+  game?: Game;
+
+  ngOnInit(): void { //ngOnInit: wird aufgerufen, nachdem Komp. vollständig initialisiert wurde. void: gibt keinen Wert zurück; kein return-Wert erwartet
+    this.newGame();
+  }
+
+  newGame() {
+    this.game = new Game();
+    console.log(this.game);
+    
+  }
+
+  takeCard() {
+    this.pickCardAnimation = true;
+  }
 
 }
