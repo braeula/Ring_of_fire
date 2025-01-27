9639161ec8de47ff2219e17089c199c93ee60987
Arthur Bräul <a.braeul@live.de>
2025-01-27 18:06:46 +0100

    load game info from JSON

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import {MatCardModule} from '@angular/material/card';
 
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatButtonModule, MatIconModule,MatCardModule],
+  imports: [RouterOutlet, MatButtonModule, MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
